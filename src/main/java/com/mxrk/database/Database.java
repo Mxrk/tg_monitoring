19a32224ae4658f855dcfd2e0830d2ce486d64f8
Mark <Mxrk@users.noreply.github.com>
2018-07-01 15:14:16 +0200

    rewrite database structure

--- src/main/java/com/mxrk/database/Database.java
+++ src/main/java/com/mxrk/database/Database.java
@@ -17,7 +17,10 @@ public class Database {
             /*
             TODO: Setup DB
 
-           CREATE TABLE `user` ( `id` INTEGER, `domain` TEXT, PRIMARY KEY(`domain`) )
+           CREATE TABLE `users` ( `userID` INTEGER, PRIMARY KEY(`userID`) )
+           CREATE TABLE subs( userID INTEGER, domainID INTEGER, FOREIGN KEY(userID) REFERENCES users(userID) FOREIGN KEY(domainID) REFERENCES domains(domainID) )
+           CREATE TABLE `domains` ( `domainID` INTEGER PRIMARY KEY AUTOINCREMENT, `status` INTEGER, `url` TEXT )
+
 
             */
         } catch (Exception e) {
@@ -32,16 +35,16 @@ public class Database {
         // Todo: if empty, return 0
         try {
             stmt = c.createStatement();
-            String sql = "SELECT * FROM user WHERE id = ?";
-            PreparedStatement st = c.prepareStatement(sql);
+            String nsql = "SELECT * FROM domains INNER JOIN subs ON subs.domainID=domains.domainID WHERE subs.userID = ?";
+            //String sql = "SELECT * FROM user WHERE id = ?";
+            PreparedStatement st = c.prepareStatement(nsql);
             st.setLong(1, id);
             ResultSet rs = st.executeQuery();
-            int i = 1;
             while (rs.next()) {
-                String domain = rs.getString("domain");
+                String domain = rs.getString("url");
                 //  text.append(i + " " + domain + "\n");
+                System.out.println(domain);
                 list.add(domain);
-                i++;
 
             }
         } catch (Exception e) {
@@ -51,36 +54,41 @@ public class Database {
     }
 
     public static int countDomains(long id) {
-        int i = 0;
+
         try {
             stmt = c.createStatement();
-            String sql = "SELECT * FROM user WHERE id = ?";
+            String sql = "SELECT count(*) FROM subs WHERE userID = ?";
             PreparedStatement st = c.prepareStatement(sql);
             st.setLong(1, id);
             ResultSet rs = st.executeQuery();
-
-            while (rs.next()) {
-                i++;
-            }
+            return rs.getInt(1);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return i;
+        return -1;
     }
 
     public static boolean add(long id, String domain) {
-        String sql = "INSERT INTO user(id,domain) VALUES(?,?)";
-
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
-            st.setLong(1, id);
-            st.setString(2, domain);
-            st.executeUpdate();
-            return true;
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(domainExists(domain)){
             return false;
+        }else {
+            addUser(id);
+
+            String sql = "INSERT INTO domains(url, status) VALUES(?,?)";
+            try {
+                PreparedStatement st = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                st.setString(1, domain);
+                st.setInt(2, 0);
+                st.executeUpdate();
+                ResultSet rs = st.getGeneratedKeys();
+                linkDomain(id,rs.getInt(1));
+                return true;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+
+            }
         }
     }
     public static ArrayList getAll() {
@@ -89,11 +97,11 @@ public class Database {
         // Todo: if empty, return 0
         try {
             stmt = c.createStatement();
-            String sql = "SELECT * FROM user";
+            String sql = "SELECT * FROM domains";
             ResultSet rs = stmt.executeQuery(sql);
 
             while (rs.next()) {
-                String domain = rs.getString("domain");
+                String domain = rs.getString("url");
                 list.add(domain);
             }
         } catch (Exception e) {
@@ -104,30 +112,69 @@ public class Database {
 
     public static void check(boolean check, String domain) {
         if(check){
-            String sql = "UPDATE user SET http = '1' WHERE domain = ?";
+            String sql = "UPDATE domains SET status = '1' WHERE url = ?";
             try {
                 PreparedStatement pstmt = c.prepareStatement(sql);
                 pstmt.setString(1, domain); // set input parameter 1
                 pstmt.executeUpdate(); // execute update statement
-                c.commit();
+               // c.commit();
             } catch (SQLException exp) {
                 exp.printStackTrace();
             }
         }else{
             try {
-                String sql = "UPDATE user SET http = '0' WHERE domain = ?";
+                String sql = "UPDATE domains SET status = '0' WHERE url = ?";
                 PreparedStatement pstmt = c.prepareStatement(sql);
                 pstmt.setString(1, domain); // set input parameter 1
                 pstmt.executeUpdate(); // execute update statement
-                c.commit();
+                //c.commit();
             } catch (SQLException exp) {
                 exp.printStackTrace();
             }
 
         }
     }
+    public static boolean domainExists(String domain) {
+        try {
+            stmt = c.createStatement();
+            String sql = "select count(1) from domains where url = ?";
+            PreparedStatement st = c.prepareStatement(sql);
+            st.setString(1, domain);
+            ResultSet rs = st.executeQuery();
+            int count = rs.getInt(1);
+            if(count == 0){
+                return false;
+            }else{
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 
+    public static void addUser(long userid){
+        String sql = "INSERT if not exists INTO users(userID) VALUES(?)";
+        try {
+            PreparedStatement st = c.prepareStatement(sql);
+            st.setLong(1, userid);
+            st.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void linkDomain(long userID, int domainID){
+        String sql = "INSERT if not exists INTO users(userID, domainID) VALUES(?,?)";
+        try {
+            PreparedStatement st = c.prepareStatement(sql);
+            st.setLong(1, userID);
+            st.setInt(2,domainID);
+            st.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public void closeConnection() {
         try {
             c.close();
